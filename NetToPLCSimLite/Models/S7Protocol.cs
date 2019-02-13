a1901188da958f24cfaf2450f6371fca26d6c74c
jybbang <drogbang@gmail.com>
2019-02-13 15:32:03 +0900

    Revert "gc call"
    This reverts commit c47e3a692da73b36d34aeeb9b33285cdf17ccf4d.

--- NetToPLCSimLite/Models/S7Protocol.cs
+++ NetToPLCSimLite/Models/S7Protocol.cs
@@ -220,7 +220,7 @@ namespace NetToPLCSimLite.Models
             // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
             Dispose(true);
             // TODO: 위의 종료자가 재정의된 경우 다음 코드 줄의 주석 처리를 제거합니다.
-            GC.SuppressFinalize(this);
+            // GC.SuppressFinalize(this);
         }
         #endregion
     }
